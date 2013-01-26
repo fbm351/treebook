142a21a0d2a0bc15a1d6559ca3ee8f23f1e0c1e6
Fred Myers <fred@fred-myers.com>
2013-01-26 16:02:27 -0500

    Added user validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter a first name" do
+      user = User.new
+      assert !user.save
+      assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+      user = User.new
+      assert !user.save
+      assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+      user = User.new
+      assert !user.save
+      assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.email = "fred@fred-myers.com"
+    user.profile_name = "fredmyers"
+    user.first_name = "Fred"
+    user.last_name = "Myers"
+    user.password = "P@ssw0rd"
+    user.password_confirmation = "P@ssw0rd"
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a profile name without spaces" do
+    user = User.new
+    user.profile_name = "My Profile With Spaces"
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("must be formatted correctly.")
+  end
 end
